e8a60bec0d908affcd99e92c8f4f602993db608c
Trenten grede <Trenten.grede@gmail.com>
2021-02-23 23:03:05 -0600

    friends list properly renders when logged in

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 function FriendsList(props) {
+     const [friendsList, setFriendsList] = useState([])
+
+     const getData = () => {
+          axiosWithAuth()
+          .get('/api/friends')
+          .then((res) => {
+               console.log(res)
+               console.log(res.data)
+               setFriendsList(res.data)
+          })
+          .catch((err) => {
+               console.log(err)
+          })
+     }
+
+     useEffect(() => {
+          getData()
+     }, [])
+
      return (
           <div>
-               FriendsList
+              {friendsList.map(friend => {
+                   return (<div>
+                   <h2>{friend.name}</h2>
+                   <h3>{friend.email}</h3>
+                   <h4>{friend.age}</h4>
+                   </div>)
+              })}
           </div>
      )
 }
